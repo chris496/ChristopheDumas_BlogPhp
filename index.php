4fea9316214cfcc1b535bd6db29d199cca2da159
Christophe <christophe.dumas1@gmail.com>
2022-01-21 17:17:21 +0100

    📦 NEW: redirect to all posts controller

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-
+use app\blog\Controller\Post;
 
 require('./vendor/autoload.php');
 
-require './app/Controller/Post.php';
-
-$test = new Post();
-$dis = $test->allPosts();
-var_dump($dis);
+if (isset($_GET['action'])){
+    // Display all posts
+    if ($_GET['action'] == 'allposts'){
+        $allPosts = new Post();
+        $allPosts->allPosts();
+    }
+}
+else{
+    $allPosts = new Post();
+    $allPosts->allPosts();
+}
