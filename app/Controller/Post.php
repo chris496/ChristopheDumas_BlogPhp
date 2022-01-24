c58f8931171e2aa0a9f73b86f273439a854d7d3e
Christophe <christophe.dumas1@gmail.com>
2022-01-24 18:59:53 +0100

    📦 NEW: controller add new post

--- app/Controller/Post.php
+++ app/Controller/Post.php
@@ -18,12 +18,19 @@ class Post extends Controller{
     }
 
      //display a selected post
-     public function getOnePost(){
-         $postManager = new PostManager();
-         $post = $postManager->getPost($_GET['id']);
-         $this->twig->display('onePost.html.twig', [
+    public function getOnePost(){
+        $postManager = new PostManager();
+        $post = $postManager->getPost($_GET['id']);
+        $this->twig->display('onePost.html.twig', [
             'post' => $post
         ]);
         return $post;
-     }
+    }
+
+    //create a new post
+    public function createPost($id, $title, $chapo, $description){
+        $postManager = new PostManager();
+        $postManager->createPost($id, $title, $chapo, $description);
+         header('Location: index.php');
+    }
 }
