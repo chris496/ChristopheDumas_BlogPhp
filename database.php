165e3d59f75ab8e185c47aaf5b6360c05f7ac5f4
Christophe <christophe.dumas1@gmail.com>
2022-01-28 16:11:00 +0100

    👌 IMPROVE:  create dotenv for dbConnect

--- database.php
+++ database.php
@@ -1,10 +1,23 @@
 <?php
+//use PDO;
+use Symfony\Component\Dotenv\Dotenv;
 
 function dbConnect()
 {
+    $dotenv = new Dotenv();
+    $dotenv->load(__DIR__.'/.env');
+    $dbhost = $_ENV['DB_HOST'];
+    $dbname = $_ENV['DB_NAME'];
+    $username = $_ENV['DB_USERNAME'];
+    $password = $_ENV['DB_PASSWORD'];
+
     try
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=blogphp;charset=utf8', 'root', 'Sql@42ocBdd496');
+        $pdo = new PDO(
+            "mysql:host=$dbhost;dbname=$dbname;charset=utf8", 
+            "$username", 
+            "$password"
+        );
         return $pdo;
     }
     catch(Exception $e)
