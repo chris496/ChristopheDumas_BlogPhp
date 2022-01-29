fb0cd51aaf984c02646af2ace605f600a4c3ebff
Christophe <christophe.dumas1@gmail.com>
2022-01-29 17:56:57 +0100

    📦 NEW: create class comment controller+function

---
+++ app/Controller/Comment.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\blog\Controller;
+
+use App\blog\Model\CommentManager;
+
+class comment
+{
+    //create a new comment
+    public function createComment($postId, $pseudo, $email, $description)
+    {
+        $commentManager = new CommentManager();
+        $newComment = $commentManager->postComment($postId, $pseudo, $email, $description);
+        header('Location: index.php?action=getOnePost&id=' . $postId);
+    }
+}
--- app/Model/CommentManager.php
+++ app/Model/CommentManager.php
@@ -5,7 +5,7 @@ namespace App\blog\Model;
 class CommentManager extends Model
 {
     //create a new comment
-    public function postComment($postId, $pseudo, $email, $description, $added_date)
+    public function postComment($postId, $pseudo, $email, $description)
     {
         $req = $this->db->prepare('INSERT INTO comment(fk_post_id, pseudo, email, description, added_date) VALUES (:fk_post_id, :pseudo, :email, :description, :added_date)');
         $newComment = $req->execute(array(
