4992e65b0c3cd4df0ac2b5b237a673be445fcf63
Christophe <christophe.dumas1@gmail.com>
2022-02-01 23:49:36 +0100

    📦 NEW: create model + function userRegistration

---
+++ app/Model/UserManager.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\blog\Model\Model;
+
+class UserManager extends Model
+{
+    //user registration
+    public function userRegistration($firstname, $lastname, $email, $password)
+    {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $req = $this->db->prepare('INSERT INTO user(firstname, lastname, email, password) VALUES(?,?,?,?,?)');
+        $newUser = $req->execute(array(
+            $firstname, 
+            $lastname, 
+            $email, 
+            $hash,
+        ));
+        return $newUser;
+    }
+}
