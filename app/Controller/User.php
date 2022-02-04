fd7913ceb309455cd8c5c86d1beaae0f966f0b82
Christophe <christophe.dumas1@gmail.com>
2022-02-04 15:51:44 +0100

    📦 NEW: add function controller userLogin

--- app/Controller/User.php
+++ app/Controller/User.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\blog\Controller;
 
+use App\blog\Model\PostManager;
 use App\blog\Model\UserManager;
 
 class User extends Controller
 {
-     //registration
-     public function pageRegistration()
+    //page registration
+    public function pageRegistration()
     {
         $this->twig->display('registration.html.twig');
     }
@@ -16,5 +17,40 @@ class User extends Controller
         $userManager = new UserManager();
         $userManager->userRegistration($lastname, $firstname, $email, $password);
         header('Location: index.php');
+    }
+
+    //page login
+    public function pageLogin()
+    {
+        $this->twig->display('login.html.twig');
+    }
+
+    //user login
+    public function userLogin($email, $password)
+    {
+        $postsManager = new PostManager();
+        $posts = $postsManager->getPosts();
+        
+        $userManager = new UserManager();
+        $newLogin = $userManager->userLogin($email, $password);
+        if (password_verify($password, $newLogin['password']))
+        {
+            session_start();
+            $_SESSION['firstname'] = $newLogin['firstname'];
+            $_SESSION['lastname'] = $newLogin['lastname'];
+            $_SESSION['email'] = $newLogin['email'];
+            $_SESSION['role'] = $newLogin['role'];
+            $this->twig->display('index.html.twig', [
+                'posts' => $posts,
+                'firstname' => $_SESSION['firstname'],
+                'lastname' => $_SESSION['lastname'],
+                'email' => $_SESSION['email'],
+                'role' => $_SESSION['role']
+            ]);
+        }
+        else
+        {
+            echo ('connexion refusé');
+        }
     }
 }
