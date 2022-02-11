e6a6c43f2fe301911e0d5279f131734d1a3448fd
Christophe <christophe.dumas1@gmail.com>
2022-02-11 23:10:41 +0100

    📦 NEW: add file management in controller function

--- app/Controller/Post.php
+++ app/Controller/Post.php
@@ -43,10 +43,28 @@ class Post extends Controller
     //create a new post
     public function createPost($title, $chapo, $description)
     {
+        
+        if (isset($_FILES['photo']) && $_FILES['photo']['error'] == 0)
+        {
+            if ($_FILES['photo']['size'] <= 4000000)
+            {
+                $fileInfo = pathinfo($_FILES['photo']['name']);
+                $extension = $fileInfo['extension'];
+                $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+                if (in_array($extension, $allowedExtensions))
+                {
+                    $uniqueName = uniqid('', true);
+                    $file = $uniqueName.".".$extension;
+                    move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/' . basename($file));
+                    echo "L'envoi a bien été effectué !";
+                }
+            }
+        };
+
         $user = $this->isAdmin();
         $id = $user['id'];
         $postManager = new PostManager();
-        $postManager->createPost($id, $title, $chapo, $description);
+        $postManager->createPost($id, $title, $chapo, $description, $file);
          header('Location: index.php');
     }
 
@@ -64,11 +82,32 @@ class Post extends Controller
         return $post;
     }
 
-    //page update post
+    //update a post
     public function updatePost($id, $title, $chapo, $description)
     {
         $postManager = new PostManager();
-        $postManager->updatePost($id, $title, $chapo, $description);
+        $post = $postManager->getPost($id);
+
+        if (isset($_FILES['photo']) && $_FILES['photo']['error'] == 0)
+        {
+            $fichier = './uploads/' .$post['picture'];
+            if(file_exists($fichier)){unlink($fichier);}
+            if ($_FILES['photo']['size'] <= 4000000)
+            {
+                $fileInfo = pathinfo($_FILES['photo']['name']);
+                $extension = $fileInfo['extension'];
+                $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+                if (in_array($extension, $allowedExtensions))
+                {
+                    $uniqueName = uniqid('', true);
+                    $file = $uniqueName.".".$extension;
+                    move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/' . basename($file));
+                    echo "L'envoi a bien été effectué !";
+                }
+            }
+        };
+        
+        $postManager->updatePost($id, $title, $chapo, $description, $file);
         header('Location: index.php');
     }
 
@@ -76,7 +115,25 @@ class Post extends Controller
     public function deletePost()
     {
         $postManager = new PostManager();
+        $post = $postManager->getPost($_GET['id']);
+
+        $fichier = './uploads/' .$post['picture'];
+        if(file_exists($fichier)){unlink($fichier);}
+        
         $postManager->deletePost($_GET['id']);
         header('Location: index.php?action=pageAdministration');
+    }
+
+    //delete a picture
+    public function deletePicture()
+    {
+        $postManager = new PostManager();
+        $post = $postManager->getPost($_GET['id']);
+
+        $fichier = './uploads/' .$post['picture'];
+        if(file_exists($fichier)){unlink($fichier);}
+        
+        $postManager->deletePicture($_GET['id']);
+        header('Location: index.php?action=pageUpdatePost&id=' .$_GET['id']);
     }
 }
