c099fa915cbe183b286ca0fefba9926df19e8215
Christophe <christophe.dumas1@gmail.com>
2022-02-11 23:11:02 +0100

    📦 NEW: add deletePicture in model

--- app/Model/PostManager.php
+++ app/Model/PostManager.php
@@ -19,35 +19,38 @@ class PostManager extends Model
     //display a selected post
     public function getPost($postId)
     {
-        $req = $this->db->prepare('SELECT lastname, firstname, post.id, title, chapo, description, post.added_date, post.update_date FROM user INNER JOIN post ON user.id = post.fk_user_id WHERE post.id = :id' );
+        $req = $this->db->prepare('SELECT lastname, firstname, post.id, title, chapo, picture, description, post.added_date, post.update_date FROM user INNER JOIN post ON user.id = post.fk_user_id WHERE post.id = :id' );
         $req->execute(array('id'=> $postId));
         $post = $req->fetch();
         return $post;
     }
 
     //create a new post
-    public function createPost($id, $title, $chapo, $description)
+    public function createPost($id, $title, $chapo, $description, $file)
     {
-        $req = $this->db->prepare('INSERT INTO post(fk_user_id, title, chapo, description, added_date) VALUES(:fk_user_id, :title, :chapo, :description, :create_at)');
+        //dd($id, $title, $chapo, $description, $file);
+        $req = $this->db->prepare('INSERT INTO post(fk_user_id, title, chapo, description, picture, added_date) VALUES(:fk_user_id, :title, :chapo, :description, :picture, :create_at)');
         $newPost = $req->execute(array(
             'fk_user_id'=> $id,
             'title'=> $title,
             'chapo'=> $chapo,
             'description'=> $description,
+            'picture'=> $file,
             ':create_at'=> date('Y-m-d H:i:s')
         ));
         return $newPost;
     }
 
     //update a post
-    public function updatePost($id, $title, $chapo, $description)
+    public function updatePost($id, $title, $chapo, $description, $file)
     {
-        $req = $this->db->prepare('UPDATE post SET title = :title, chapo = :chapo, description = :description, update_date = :update_date WHERE id = :id');
+        $req = $this->db->prepare('UPDATE post SET title = :title, chapo = :chapo, description = :description, picture = :picture, update_date = :update_date WHERE id = :id');
         $updatePost = $req->execute(array(
             'id'=> $id,
             'title'=> $title,
             'chapo'=> $chapo,
             'description'=> $description,
+            'picture'=> $file,
             'update_date' => date('Y-m-d H:i:s')
         ));
         return $updatePost;
@@ -57,6 +60,15 @@ class PostManager extends Model
         $req = $this->db->prepare('DELETE FROM post WHERE id = :id');
         $deletePost = $req->execute(array(
             'id'=> $id,
+            ));
+        return $deletePost;
+    }
+    //delete a picture
+    public function deletePicture($id){
+        $req = $this->db->prepare('UPDATE post SET picture = :picture WHERE id = :id');
+        $deletePost = $req->execute(array(
+            'id'=> $id,
+            'picture'=> ''
             ));
         return $deletePost;
     }
